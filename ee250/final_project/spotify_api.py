e8e5301e3fd53229a7b7091ed05314c09aa5c5db
Haowen Liu <liu.haowen.andy@gmail.com>
2020-11-10 18:04:27 -0800

    Reorganize API calls

--- ee250/final_project/spotify_api.py
+++ ee250/final_project/spotify_api.py
@@ -7,23 +7,60 @@ import spotipy.util as util
 from json.decoder import JSONDecodeError
 
 spotifyObject = None
+deviceID = None
+tracks = []
 
 
 def spotify_init():
     global spotifyObject
+    global deviceID
 
     # Get the username from terminal
     username = sys.argv[1]
+
+    client_id = '9b907c9f2ad4424792057ba71606f879'  # placeholder value here
+    client_secret = '8429b7ab10e144a0b3fcb9ab00ae90cf'  # placeholder value here
+    redirect_uri = 'https://www.google.com'
     scope = 'user-read-private user-read-playback-state user-modify-playback-state'
 
     try:
-        token = util.prompt_for_user_token(username, scope)
+        token = util.prompt_for_user_token(
+            username, scope, client_id, client_secret, redirect_uri)
     except (AttributeError, JSONDecodeError):
         os.remove(f".cache-{username}")
-        token = util.prompt_for_user_token(username, scope)
+        token = util.prompt_for_user_token(
+            username, scope, client_id, client_secret, redirect_uri)
 
     spotifyObject = spotipy.Spotify(auth=token)
+
+    devices = spotifyObject.devices()
+    deviceID = devices['devices'][0]['id']
 
 
 def spotify_search(query):
     return spotifyObject.search(query, 5)
+
+
+def spotify_play(track_id):
+    global deviceID
+    global spotifyObject
+    global tracks
+    track = spotifyObject.track(track_id)
+    tracks = [track]
+    spotifyObject.start_playback(deviceID, None, tracks)
+
+
+def spotify_set_volume(volume):
+    global deviceID
+    global spotifyObject
+    spotifyObject.volume(volume, deviceID)
+
+
+def spotify_playpause(playpause):
+    global deviceID
+    global spotifyObject
+    global tracks
+    if playpause:
+        spotifyObject.start_playback(deviceID, None, tracks)
+    else:
+        spotifyObject.pause_playback(deviceID)
--- ee250/final_project/spotify_int.py
+++ ee250/final_project/spotify_int.py
@@ -1,13 +1,6 @@
 # Import libraries
-import os
-import sys
-import json
-import spotipy
-import webbrowser
-import spotipy.util as util
-from pprint import pprint
 import paho.mqtt.client as mqtt
-from json.decoder import JSONDecodeError
+import spotify_api
 
 
 def on_connect(client, userdata, flags, rc):
@@ -47,96 +40,11 @@ def on_playpausee(client, userdata, msg):
     pass
 
 
+spotify_api.spotify_init()
+
+
 client = mqtt.Client()
 client.on_message = on_message
 client.on_connect = on_connect
 client.connect(host="eclipse.usc.edu", port=11000, keepalive=60)
 client.loop_start()
-
-
-username = 'm8p86s9tyv0wxt4izw49c3i79'  # placeholder value here
-client_id = '9b907c9f2ad4424792057ba71606f879'  # placeholder value here
-client_secret = '8429b7ab10e144a0b3fcb9ab00ae90cf'  # placeholder value here
-redirect_uri = 'https://www.google.com'
-scope = 'user-read-private user-read-playback-state user-modify-playback-state'
-
-token = util.prompt_for_user_token(
-    username, scope, client_id, client_secret, redirect_uri)
-
-sp = spotipy.Spotify(auth=token)
-
-devices = sp.devices()
-#print(json.dumps(devices, sort_keys=True, indent=4))
-deviceID = devices['devices'][0]['id']
-
-# Get track information
-#track = sp.current_user_playing_track()
-#print(json.dumps(track, sort_keys=True, indent=4))
-# print()
-#artist = track['item']['artists'][0]['name']
-#track = track['item']['name']
-
-# if artist !="":
-#print("Currently playing " + artist + " - " + track)
-
-# User information
-user = sp.current_user()
-displayName = user['display_name']
-
-while True:
-    print()
-    print("Welcome to Spotify " + displayName)
-    print()
-    print("Enter 0 to search for an artist")
-    print("Enter 1 to exit")
-    print()
-    choice = input("Enter your choice: ")
-
-    if choice == "0":
-        print()
-        searchQuery = input("Ok, what's their name?:")
-        print()
-        # Get search results
-        searchResults = sp.search(searchQuery, 1, 0, "artist")
-        # Print artist details
-        artist = searchResults['artists']['items'][0]
-        print(artist['name'])
-        print(artist['genres'][0])
-        print()
-        webbrowser.open(artist['images'][0]['url'])
-        artistID = artist['id']
-        # Album details
-        trackURIs = []
-        trackArt = []
-        z = 0
-
-        # Extract data from album
-        albumResults = sp.artist_albums(artistID)
-        albumResults = albumResults['items']
-
-        for item in albumResults:
-            print("ALBUM: " + item['name'])
-            albumID = item['id']
-            albumArt = item['images'][0]['url']
-
-            # Extract track data
-            trackResults = sp.album_tracks(albumID)
-            trackResults = trackResults['items']
-
-            for item in trackResults:
-                print(str(z) + ": " + item['name'])
-                trackURIs.append(item['uri'])
-                trackArt.append(albumArt)
-                z += 1
-            print()
-        while True:
-            songSelection = input("Enter a song number to see the album art: ")
-            if songSelection == "x":
-                break
-            trackSelectionList = []
-            trackSelectionList.append(trackURIs[int(songSelection)])
-            sp.start_playback(deviceID, None, trackSelectionList)
-            webbrowser.open(trackArt[int(songSelection)])
-    # End program
-    if choice == "1":
-        break
